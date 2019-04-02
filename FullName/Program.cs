1dc83890e3f197a755c64c5255b783d1e5309fed
Erin Moore <erin.moore726@gmail.com>
2019-04-01 19:44:58 -0500

    FullName in progress

--- FullName/Program.cs
+++ FullName/Program.cs
@@ -6,13 +6,33 @@ namespace FullName
     {
         public static void Main()
         {
+            
+
             // TODO: Ask the user for their first name and then their last name.
 
             // TODO: Call a method called FullName that returns their full name.
 
             // TODO: Output their full name.
 
+            FullName();
+
             Console.ReadLine();
+        }
+
+        public static void FullName()
+        {
+            
+
+            Console.Write("What's your first name? ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What's your last name? ");
+            string lastName = Console.ReadLine();
+
+            string message = (firstName) + " " + (lastName);
+
+            Console.Write(message);
+            
         }
 
         // TODO: Create a method call FullName that takes the firstName and lastName as parameters.
